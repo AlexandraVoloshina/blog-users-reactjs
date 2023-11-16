867d958998d3a57c4a3149af6a09549b2f6be8da
Alexandra Voloshina <alexandravoloshina@Alexandras-MacBook-Pro.local>
2023-11-16 18:28:59 +0200

    secons commit

---
+++ src/components/Users.js
@@ -0,0 +1,25 @@
+import React from "react"
+import User from "./User"
+
+class Users extends React.Component {
+    
+    render() {
+        if(this.props.users.length > 0)
+            return (
+                <div>
+                    {this.props.users.map((el) => (
+                        <User onDelete={this.onDelete} key={el.id} user={el}/>
+                    ))
+                    }
+                </div>
+            )
+        else
+            return (
+                <div className="user">
+                    <h3>Користувачів нема</h3>
+                </div>
+            )
+    }
+}
+
+export default Users
